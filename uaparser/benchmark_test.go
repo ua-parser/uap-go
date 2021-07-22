d23a1589a211bab3629808dcc1f6f1e487e5da0f
Dongdong Yang <dongdong@servicewall.ai>
2021-07-22 17:38:42 +0800

    feat: support UA fingerprint quick talel lookup, performance is 2500x vs origin parse

--- uaparser/benchmark_test.go
+++ uaparser/benchmark_test.go
@@ -36,6 +36,40 @@ func BenchmarkParserWithOptions(b *testing.B) {
 	}
 }
 
+func BenchmarkParserFp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, ua := range uas {
+			benchedParser.ParseFp(ua)
+		}
+	}
+}
+
+func TestParserFp(t *testing.T) {
+	for _, ua := range uas {
+		if benchedParser.ParseFp(ua).Os.Family != benchedParser.Parse(ua).Os.Family {
+			t.Errorf("Fp lookup not equal to Regex lookup" )
+		}
+		if benchedParser.ParseFp(ua).UserAgent.Family != benchedParser.Parse(ua).UserAgent.Family {
+			t.Errorf("Fp lookup not equal to Regex lookup")
+		}
+		if benchedParser.ParseFp(ua).Device.Family != benchedParser.Parse(ua).Device.Family {
+			t.Errorf("Fp lookup not equal to Regex lookup")
+		}
+	}
+
+	for _, ua := range top {
+		if benchedParser.ParseFp(ua).Os.Family != benchedParser.Parse(ua).Os.Family {
+			t.Errorf("Fp lookup not equal to Regex lookup" )
+		}
+		if benchedParser.ParseFp(ua).UserAgent.Family != benchedParser.Parse(ua).UserAgent.Family {
+			t.Errorf("Fp lookup not equal to Regex lookup")
+		}
+		if benchedParser.ParseFp(ua).Device.Family != benchedParser.Parse(ua).Device.Family {
+			t.Errorf("Fp lookup not equal to Regex lookup")
+		}
+	}
+}
+
 var uas = []string{
 	"Mozilla/5.0 (Windows NT 6.0; rv:2.0b6pre) Gecko/20100907 Firefox/4.0b6pre",
 	"Mozilla/5.0 (Windows NT 5.2; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
@@ -50,4 +84,55 @@ var uas = []string{
 	"BlackBerry7730	3.7.1	BlackBerry7730/3.7.1 UP.Link/5.1.2.5",
 	"Galeon	1.3.18	Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.3) Gecko/20041020 Galeon/1.3.18",
 	"Opera/9.80 (J2ME/MIDP; Opera Mini/7.1.32052/35.5706; U; id) Presto/2.8.119 Version/11.10",
+}
+
+var top = []string{
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
+	"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Mobile Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/91.0.4469.0 Safari/537.36",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148szairApp",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36 Hutool",
+	"Java/1.8.0_221",
+	"Apache-HttpClient/4.1.2 (java 1.5)",
+	"pyspider/0.3.10 (+http://pyspider.org/)",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.4.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.9(0x18000924) NetType/WIFI Language/zh_CN",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.9(0x18000924) NetType/4G Language/zh_CN",
+	"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36 Edg/91.0.864.71",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Mobile/15E148 Safari/604.1",
+	"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36 SE 2.X MetaSr 1.0",
+	"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 SE 2.X MetaSr 1.0",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36 SE 2.X MetaSr 1.0",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+	"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/527.36",
+	"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0",
+	"Mozilla/5.0 (Linux; U; Android 9; zh-cn; MI 6 Build/PKQ1.190118.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141 Mobile Safari/537.36 XiaoMi/MiuiBrowser/12.8.25",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; SV1; .NET CLR 2.0.1124)",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+	"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)",
+	"axios/0.17.1",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.3.4000 Chrome/30.0.1599.101 Safari/537.36",
+	"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+	"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 SE 2.X MetaSr 1.0",
+	"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36",
 }
---
+++ uaparser/fp.go
@@ -0,0 +1,74 @@
+package uaparser
+
+var fpDict = map[string]*Client {
+    // Windows, Chrome
+    "MozillaWindowsNTWinxAppleWebKitKHTMLlikeGeckoChromeSafari": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Chrome", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaWindowsNTWOWAppleWebKitKHTMLlikeGeckoChromeSafari": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Chrome", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaWindowsNTWOWAppleWebKitKHTMLlikeGeckoChromeTaoBrowserSafari": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Chrome", "", "", "", },Device: &Device{"Other","",""}},
+    // Windows, Edge/IE
+    "MozillaWindowsNTWinxAppleWebKitKHTMLlikeGeckoChromeSafariEdg": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Edge", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaWindowsNTWOWTridentrvlikeGecko": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"IE", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillacompatibleMSIEWindowsNTTrident": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"IE", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillacompatibleMSIEWindowsNTNETCLRNETCLR": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"IE", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillacompatibleMSIEWindowsNTSVNETCLR":{Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"IE", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillacompatibleMSIEWindowsNT": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"IE", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillacompatibleMSIEWindowsNTTencentTraveler":{Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"IE", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillacompatibleMSIEWindowsNTWOWTridentSLCCNETCLRNETCLRNETCLRMediaCenterPCNETCNETE":{Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"IE", "", "", "", },Device: &Device{"Other","",""}},
+    // Windows, Sogou
+    "MozillaWindowsNTWOWAppleWebKitKHTMLlikeGeckoChromeSafariSEXMetaSr": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Sogou Explorer", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaWindowsNTAppleWebKitKHTMLlikeGeckoChromeSafariSEXMetaSr": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Sogou Explorer", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaWindowsNTWOWAppleWebKitKHTMLlikeGeckoMaxthonChromeSafari": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Maxthon", "", "", "", },Device: &Device{"Other","",""}},
+    // Mac OS X, Chrome
+    "MozillaMacintoshIntelMacOSXAppleWebKitKHTMLlikeGeckoChromeSafari": {Os: &Os{	 "Mac OS X", "","","",""}, UserAgent: &UserAgent{"Chrome", "", "", "", },Device: &Device{"Mac","",""}},
+    // Mac OS X, Firefox
+    "MozillaMacintoshIntelMacOSXrvGeckoFirefox": {Os: &Os{	 "Mac OS X", "","","",""}, UserAgent: &UserAgent{"Firefox", "", "", "", },Device: &Device{"Mac","",""}},
+    // Mobile
+    "MozillaLinuxUAndroidzhcnMIBuildPKQAppleWebKitKHTMLlikeGeckoVersionChromeMobileSafariXiaoMiMiuiBrowser": {Os: &Os{	 "Android", "","","",""}, UserAgent: &UserAgent{"MiuiBrowser", "", "", "", },Device: &Device{"XiaoMi MI 6","",""}},
+    "MozillaLinuxAndroidNexusBuildMRANAppleWebKitKHTMLlikeGeckoChromeMobileSafari": {Os: &Os{	 "Android", "","","",""}, UserAgent: &UserAgent{"Chrome Mobile", "", "", "", },Device: &Device{"Nexus 5","",""}},
+    "MozillaWindowsNTWOWAppleWebKitKHTMLlikeGeckoHeadlessChromeSafari": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"HeadlessChrome", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaiPhoneCPUiPhoneOSlikeMacOSXAppleWebKitKHTMLlikeGeckoMobileE": {Os: &Os{	 "iOS", "","","",""}, UserAgent: &UserAgent{"Mobile Safari UI/WKWebView", "", "", "", },Device: &Device{"iPhone","",""}},
+    "MozillaiPhoneCPUiPhoneOSlikeMacOSXAppleWebKitKHTMLlikeGeckoVersionMobileESafari": {Os: &Os{	 "iOS", "","","",""}, UserAgent: &UserAgent{"Mobile Safari", "", "", "", },Device: &Device{"iPhone","",""}},
+    "MozillaWindowsNTWOWAppleWebKitKHTMLlikeGeckoChromeSafariMicroMessengerNetTypeWIFIMiniProgramEnvWindowsWindowsWechat": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Chrome", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaiPhoneCPUiPhoneOSlikeMacOSXAppleWebKitKHTMLlikeGeckoMobileEMicroMessengerxNetTypeWIFILanguagezhCN": {Os: &Os{	 "iOS", "","","",""}, UserAgent: &UserAgent{"Mobile Safari UI/WKWebView", "", "", "", },Device: &Device{"iPhone","",""}},
+    "MozillaiPhoneCPUiPhoneOSlikeMacOSXAppleWebKitKHTMLlikeGeckoMobileEMicroMessengerxNetTypeGLanguagezhCN": {Os: &Os{	 "iOS", "","","",""}, UserAgent: &UserAgent{"Mobile Safari UI/WKWebView", "", "", "", },Device: &Device{"iPhone","",""}},
+    // customer special
+    "MozillaiPhoneCPUiPhoneOSlikeMacOSXAppleWebKitKHTMLlikeGeckoMobileEszairApp": {Os: &Os{	 "iOS", "","","",""}, UserAgent: &UserAgent{"Mobile Safari UI/WKWebView", "", "", "", },Device: &Device{"iPhone","",""}},
+    "MozillaWindowsNTWinxAppleWebKitKHTMLlikeGeckoChromeSafariHutool": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Chrome", "", "", "", },Device: &Device{"Other","",""}},
+    // tools
+    "Java": {Os: &Os{	 "Other", "","","",""}, UserAgent: &UserAgent{"Java", "", "", "", },Device: &Device{"Spider","",""}},
+    "ApacheHttpClientjava": {Os: &Os{	 "Other", "","","",""}, UserAgent: &UserAgent{"Apache-HttpClient", "", "", "", },Device: &Device{"Other","",""}},
+    "pyspiderhttppyspiderorg": {Os: &Os{	 "Other", "","","",""}, UserAgent: &UserAgent{"pyspider", "", "", "", },Device: &Device{"Spider","",""}},
+    "axios": {Os: &Os{	 "Other", "","","",""}, UserAgent: &UserAgent{"axios", "", "", "", },Device: &Device{"Other","",""}},
+    // test cases 10
+    "MozillaWindowsNTrvbpreGeckoFirefoxbpre":{Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Firefox Beta", "", "", "", },Device: &Device{"Other","",""}},
+    "MozillaWindowsNTrvGeckoFirefox": {Os: &Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Firefox", "", "",""},Device: &Device{"Other","",""}},
+    "MozillaWindowsUWindowsNTfrrvGeckoFirefoxGTB": {Os:&Os{	 "Windows", "","","",""}, UserAgent: &UserAgent{"Other", "", "",""},Device: &Device{"Other","",""}},
+    "MozillaAndroidLinuxarmvlrvbpreGeckoFirefoxbpreFennecbpre": {Os:&Os{	 "Android", "","","",""}, UserAgent: &UserAgent{"Firefox Mobile", "", "",""},Device: &Device{"Generic Smartphone","Generic Smartphone",""}},
+    "MozillaXULinuxxenUSrvGeckoThunderbird": {Os:&Os{	 "Linux", "","","",""}, UserAgent: &UserAgent{"Thunderbird", "", "",""},Device: &Device{"Other","",""}},
+    "MozillaMacintoshIntelMacOSXrvGeckoFirefoxSeaMonkey": {Os:&Os{	 "Mac OS X", "","","",""}, UserAgent: &UserAgent{"SeaMonkey", "", "",""},Device: &Device{"Mac","Apple","Mac"}},
+    "ACSNFACSNFNECc": {Os:&Os{	 "Other", "","","",""}, UserAgent: &UserAgent{"Other", "", "",""},Device: &Device{"Generic Feature Phone","Generic Feature Phone",""}},
+    "BlackBerryBlackBerryUPLink": {Os:&Os{	 "BlackBerry OS", "","","",""}, UserAgent: &UserAgent{"BlackBerry", "", "",""},Device: &Device{"BlackBerry 7730","BlackBerry 7730",""}},
+    "GaleonMozillaXULinuxienUSrvGeckoGaleon": {Os:&Os{	 "Linux", "","","",""}, UserAgent: &UserAgent{"Galeon", "", "",""},Device: &Device{"Other","",""}},
+    "OperaJMEMIDPOperaMiniUidPrestoVersion": {Os:&Os{	 "Other", "","","",""}, UserAgent: &UserAgent{"Opera Mini", "", "",""},Device: &Device{"Generic Feature Phone","Generic Feature Phone",""}},
+}
+
+func (parser *Parser) GetFp(line string) string {
+    fp := make([]byte, 0)
+    for i, v := range line {
+        if v>='A' && v<='Z' || v>='a' && v<='z' {
+            fp = append(fp, line[i])
+        }
+    }
+    return string(fp)
+}
+
+func (parser *Parser) ParseFp(line string) *Client {
+    fp := parser.GetFp(line)
+    if v, ok := fpDict[fp]; ok {
+        return v
+    }
+    client := parser.Parse(line)
+    //fmt.Printf("%s: { %s, %s, %s } -> %s\n", fp, client.Os.Family, client.UserAgent.Family, client.Device.Family,  line)
+    return client
+}
+
